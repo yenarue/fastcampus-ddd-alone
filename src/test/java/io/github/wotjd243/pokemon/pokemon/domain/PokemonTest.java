2a8163ba00f1e5145901262941f1bb397b8329eb
yenarue <yenarue@gmail.com>
2020-01-19 15:12:17 +0900

    Initial commit

---
+++ src/test/java/io/github/wotjd243/pokemon/pokemon/domain/PokemonTest.java
@@ -0,0 +1,16 @@
+package io.github.wotjd243.pokemon.pokemon.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PokemonTest {
+    @Test
+    void constructor() {
+        // given
+        // when
+        // then
+        new Pokemon(1, "이상해씨", 45);
+    }
+}
